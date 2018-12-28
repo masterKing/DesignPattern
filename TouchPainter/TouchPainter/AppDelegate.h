6122c090752425575349a9ab436da622521763b8
Franky's iMac <548913086@qq.com>
2018-12-28 20:55:18 +0800

    初始化项目

---
+++ TouchPainter/TouchPainter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchPainter
+//
+//  Created by Franky on 2018/12/28.
+//  Copyright © 2018 Franky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchPainter/TouchPainter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchPainter
+//
+//  Created by Franky on 2018/12/28.
+//  Copyright © 2018 Franky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
